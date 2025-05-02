cd18f8401904c8455734f1fa48f344431334d54a
Michael Ferrari <nekkodroid404@gmail.com>
2025-05-02 20:08:14 +0200

    Adjust game logic and plot

--- mdga/__main__.py
+++ mdga/__main__.py
@@ -1,25 +1,12 @@
 from pathlib import Path
 from random import Random
 import sys
-from typing import Callable
 import matplotlib.pyplot as plt
-from collections import Counter
+from collections import deque
 
 from mdga.game import Game
 from mdga.neural_network import NeuralNetworkPlayer
 from mdga.player import FurthestPlayer, KnockoutPlayer, NearestPlayer, RandomPlayer, Player
-
-
-def plot_counter(counter: Counter, title: str, plot) -> None:
-    names = list(counter.keys())
-    count = list(counter.values())
-
-    plot.clear()
-    plot.bar(names, count)
-    plot.set_title(title)
-    plot.set_xticks(range(len(names)))
-    plot.set_xticklabels(names, rotation=20)
-    plot.set_ylim(0, max(count) * 1.1 if count else 1)
 
 
 NN_MODEL_PATH = Path("./mdga.pt")
@@ -32,43 +19,59 @@ def main() -> None:
     if NN_MODEL_PATH.exists():
         nn_player.load(NN_MODEL_PATH)
 
-    PLAYER_TYPES: list[Callable[[], Player]] = [
-        lambda: nn_player,
-        FurthestPlayer,
-        NearestPlayer,
-        lambda: RandomPlayer(random),
-        lambda: KnockoutPlayer(FurthestPlayer()),
-        lambda: KnockoutPlayer(NearestPlayer()),
-        lambda: KnockoutPlayer(RandomPlayer(random)),
+    PLAYER_TYPES: list[Player] = [
+        nn_player,
+        FurthestPlayer(),
+        NearestPlayer(),
+        RandomPlayer(random),
+        KnockoutPlayer(FurthestPlayer()),
+        KnockoutPlayer(NearestPlayer()),
+        KnockoutPlayer(RandomPlayer(random)),
     ]
 
-    # List of rounds with the player types and the winner index
-    games_won = Counter()
-    games_played = Counter()
+    histories: dict[Player, deque[bool]] = {
+        player: deque(maxlen=1000) for player in PLAYER_TYPES
+    }
 
-    fig, (plot_games_played, plot_games_won) = plt.subplots(ncols=2)
+    averages: dict[Player, list[float]] = {
+        player: list() for player in PLAYER_TYPES
+    }
+
+    fig, plot = plt.subplots()
     while plt.fignum_exists(fig.number):
         game = Game(
-            random.choice(PLAYER_TYPES)(),
-            random.choice(PLAYER_TYPES)(),
-            random.choice(PLAYER_TYPES)(),
-            random.choice(PLAYER_TYPES)(),
+            *random.choices(PLAYER_TYPES, k=4),
             random=random,
         )
 
         winner = game.play()
-        nn_player.learn(winner.decisions)
-        nn_player.decisions.clear()
 
-        games_played.update(map(str, game.players))
-        games_won.update([str(winner)])
+        for player in game.players:
+            histories[player].append(winner == player)
+            averages[player].append(sum(histories[player]) / len(histories[player]))
 
-        # TODO: Plot game win percentage for each player type and include another chart to show the player type against
-        # which the type lost.
-        plot_counter(games_played, "Player Performance: Number of Games Played by Player Type", plot_games_played)
-        plot_counter(games_won, "Player Performance: Number of Wins by Player Type", plot_games_won)
+        plot.clear()
+        plot.set_title("Average winrate of players")
+        plot.set_xlabel("Games played")
+        plot.set_ylabel("Winrate")
+
+        for player in PLAYER_TYPES:
+            plot.plot(averages[player], label=str(player))
+
+        plot.set_ylim(0, 1)
+        plot.set_xlim(
+            min(map(len, averages.values())) * 0.9,
+            max(map(len, averages.values())) * 1.1,
+        )
+        plot.legend()
+        plot.grid()
+
         fig.tight_layout()
         plt.pause(0.1)
+
+        nn_player.learn(winner.decisions)
+        for player in game.players:
+            player.decisions.clear()
 
     nn_player.save(NN_MODEL_PATH)
 
